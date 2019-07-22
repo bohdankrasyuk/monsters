9ee8549cc17eceae7415291e17a4c956cd10900a
bohdan.krasiuk <bohdan.krasiuk@oa.edu.ua>
2019-07-23 00:47:45 +0300

    Updates

---
+++ precache-manifest.e384029c6c76bc9c9632679d16732cdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcbeb347c49a2c0c7651d840c607d82",
+    "url": "/react-monsters-demo/index.html"
+  },
+  {
+    "revision": "279fc64a885849a013d5",
+    "url": "/react-monsters-demo/static/css/main.9f9708d2.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monsters-demo/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "279fc64a885849a013d5",
+    "url": "/react-monsters-demo/static/js/main.a50dcb8e.chunk.js"
+  },
+  {
+    "revision": "11752346a6ad83d7d52c",
+    "url": "/react-monsters-demo/static/js/runtime~main.6cd008be.js"
+  }
+]);
